656faed21849127a59b4d322a86fae868e4f4da4
silent27121984 <sergey27121984@gmail.com>
2023-01-18 17:53:06 +0300

    added check for division by zero

--- src/Calculator.java
+++ src/Calculator.java
@@ -9,11 +9,10 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> (x == 0 || y ==0) ? 0 : (x / y);
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
-
     Predicate<Integer> isPositive = x -> x > 0;
 
     Consumer<Integer> println = System.out::println;
--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        int c = calc.devide.apply(a, b);
-
-        calc.println.accept(a);
+        int c = calc.devide.apply(a, b);//делить на ноль нельзя, добовляем проверку (x == 0 || y ==0) ? 0 : (x / y);
+        calc.println.accept(c);
     }
 }
 
